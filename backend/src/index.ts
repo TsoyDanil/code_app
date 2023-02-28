fc949fb2644fe1a4d8ac2c300234b58f6b422cb0
danil tsoy <abdullaevdanil2002@gmail.com>
2023-02-28 20:00:12 +0600

    #2 added working backend. Tried few words to encode and decode words. orking corrctly

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express, { Express } from 'express'
 import cors from 'cors'
 import { HealthCheckController } from './controllers/healthCheck'
 import { config } from './index.config'
+import { CipherController } from './controllers/cipher'
 
 const app: Express = express()
 
@@ -9,6 +10,7 @@ const run = async () => {
     app.use(express.json())
     app.use(cors())
     app.use('/health-check', new HealthCheckController().getRouter())
+    app.use('/cipher', new CipherController().getRouter())
     app.listen(config.port, () => {
         console.log(`Server is running on port: ${config.port}`);
     })
