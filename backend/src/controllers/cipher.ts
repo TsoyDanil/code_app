fe5cf7ae052700ac9f71e4452798daf7b6e7aa25
danil tsoy <abdullaevdanil2002@gmail.com>
2023-02-28 19:54:19 +0600

    #2 added service to send encode and decode. Did not test it yet, currently planing to do so with postman

--- backend/src/controllers/cipher.ts
+++ backend/src/controllers/cipher.ts
@@ -1,4 +1,6 @@
 import express, { Router, Request, Response } from 'express'
+import IRequest from '../interfaces/IRequest';
+import IResponse from '../interfaces/IResponse';
 import { cipherService, CipherService } from "../services/cipherService";
 
 
@@ -8,11 +10,29 @@ export class CipherController {
     constructor() {
         this.router = express.Router()
         this.service = cipherService
+        this.router.post('/encode', this.encodeWord)
+        this.router.post('/decode', this.decodeWord)
     }
 
     public getRouter = (): Router => {
         return this.router
     }
 
-    
+    private encodeWord = (req: Request, res: Response): void => {
+        const request: IRequest = {
+            password: req.body.password,
+            word: req.body.word
+        }
+        const response: IResponse = this.service.encodeWord(request.password, request.word)
+        res.send(response)
+    }
+
+    private decodeWord = (req: Request, res: Response): void => {
+        const request: IRequest = {
+            password: req.body.password,
+            word: req.body.word
+        }
+        const response: IResponse = this.service.decodeWord(request.password, request.word)
+        res.send(response)
+    }
 }
