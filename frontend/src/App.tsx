9157ad8318214af0b30e1b6695edf01737016487
danil tsoy <abdullaevdanil2002@gmail.com>
2023-03-01 15:40:58 +0600

    #1 worked with styles of app + added checker on front, that will dissable btns, if one of fields (password or word) is empty

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,7 @@ import { useDispatch, useSelector, shallowEqual } from 'react-redux'
 import CipherApp from "./containers/cipherApp"
 
 const App = () => {
-
-  const dispatch: AppDispatch = useDispatch()
-
+  
   return (
     <>
       <CipherApp/>
--- frontend/src/containers/cipherApp.tsx
+++ frontend/src/containers/cipherApp.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, MouseEvent } from "react"
+import { ChangeEvent, MouseEvent, useEffect, useState } from "react"
 import { shallowEqual, useSelector } from "react-redux"
 import Loader from "../components/UI/loader/loader"
 import { changeRequestData, decodedWord, encodeWord } from "../store/cipher/cipher.slice"
@@ -8,6 +8,9 @@ import './cipherApp.css'
 
 const CipherApp = () => {
     const dispatch = useAppDispatch()
+
+    const [disableButtons, setDisableButtons] = useState<boolean>(true)
+
     const {request, response, loadingCipher} = useSelector((state: AppState) => state.cipher, shallowEqual)
 
     const handleInputRequest = (e: ChangeEvent<HTMLInputElement>) => {
@@ -27,20 +30,38 @@ const CipherApp = () => {
         }
     }
 
+    const checkButtons = () => {
+        if (request.password.trim() === '' || request.word.trim() === ''){
+            setDisableButtons(true)
+        } else{
+            setDisableButtons(false)
+        }
+    }
+
+    useEffect(() => {
+        checkButtons()
+    }, [request])
+
     return (
         <>
             <div className="cipherApp_container">
                 <h1>Cipher App</h1>
-                <input name="word" onChange={(e) => {handleInputRequest(e)}} placeholder="Add word here"/>
-                <input name="password" onChange={(e) => {handleInputRequest(e)}} placeholder="Add password here"/>
-                <div>
-                    <button name="encode" onClick={(e) => {handleButtonClick(e)}}>ENCODE</button>
-                    <button name="decode" onClick={(e) => {handleButtonClick(e)}}>DECODE</button>
+                <div className="group">      
+                    <input name="word" onChange={(e) => {handleInputRequest(e)}} placeholder={'Word'}/>
+                    <span className="highlight"></span>
+                    <span className="bar"></span>
+                </div>
+
+                <div className="group">      
+                    <input name="password" onChange={(e) => {handleInputRequest(e)}} placeholder={'Password'}/>
+                    <span className="highlight"></span>
+                    <span className="bar"></span>
+                </div>
+                <div className="cipherApp_container_button_container">
+                    <button disabled={disableButtons} name="encode" onClick={(e) => {handleButtonClick(e)}}>ENCODE</button>
+                    <button disabled={disableButtons} name="decode" onClick={(e) => {handleButtonClick(e)}}>DECODE</button>
                 </div>
                 <div>
-                    {
-                    loadingCipher ? <div>{<Loader/>}</div>
-                    : 
                     <div className="textData_container">
                         {
                             response.result.trim() !== '' ? 
@@ -52,8 +73,10 @@ const CipherApp = () => {
                             <p>{response.message}</p> :
                             <p>Additional data will be here</p>
                         }
+                        {
+                            loadingCipher ? <div>{<Loader/>}</div> : null
+                        }
                     </div>
-                    }
                 </div>
             </div>
         </>
